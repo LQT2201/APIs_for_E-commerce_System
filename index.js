92a63e7a4580d59118921a7d0b7fef4ecb32bd2e
LQT Dead Meme <luongquoctoan.ks@gmail.com>
2025-04-19 07:08:22 +0700

    deploy

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: ['http://localhost:3305', 'https://ie-307-6017b574900a.herokuapp.com'], // Allow specific origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  origin: [
+    "http://localhost:3305",
+    "https://ie-307-6017b574900a.herokuapp.com",
+  ], // Allow specific origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   credentials: true, // Allow cookies or authorization headers with requests
 };
 
@@ -26,8 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 // Kết nối với MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
@@ -39,15 +41,3 @@ app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // API routes
 app.use("/", routes);
-
-const createTextIndex = async () => {
-  try {
-    await Product.collection.createIndex({ name: "text", description: "text" });
-    console.log("Text index created successfully!");
-  } catch (error) {
-    console.error("Error creating text index:", error);
-  }
-};
-
-createTextIndex();
-
